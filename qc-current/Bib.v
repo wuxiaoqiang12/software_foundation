a0ac6384a32cda6db3b93b93eec4822fccd2e1e9
Jon Snow <1044731322@qq.com>
2019-03-17 18:27:39 +0800

    first commit

---
+++ qc-current/Bib.v
@@ -0,0 +1,12 @@
+(** * Bib: Bibliography *)
+
+(**
+
+[Wadler and Blott 1989] Philip Wadler, Stephen Blott, _How to Make
+ad-hoc Polymorphism Less ad-hoc_. POPL 1989.
+http://dl.acm.org/citation.cfm?doid=75277.75283
+
+*)
+
+
+(* Fri Mar 15 17:11:11 UTC 2019 *)
---
+++ vfa-current/SelectionTest.v
@@ -0,0 +1,124 @@
+Set Warnings "-notation-overridden,-parsing".
+From Coq Require Export String.
+From VFA Require Import Selection.
+
+Parameter MISSING: Type.
+
+Module Check.
+
+Ltac check_type A B :=
+    match type of A with
+    | context[MISSING] => idtac "Missing:" A
+    | ?T => first [unify T B; idtac "Type: ok" | idtac "Type: wrong - should be (" B ")"]
+    end.
+
+Ltac print_manual_grade A :=
+    match eval compute in A with
+    | Some (_ ?S ?C) =>
+        idtac "Score:"  S;
+        match eval compute in C with
+          | ""%string => idtac "Comment: None"
+          | _ => idtac "Comment:" C
+        end
+    | None =>
+        idtac "Score: Ungraded";
+        idtac "Comment: None"
+    end.
+
+End Check.
+
+From VFA Require Import Selection.
+Import Check.
+
+Goal True.
+
+idtac "-------------------  select_perm  --------------------".
+idtac " ".
+
+idtac "#> select_perm".
+idtac "Possible points: 3".
+check_type @select_perm (
+(forall (x : nat) (l : list nat),
+ let (y, r) := select x l in @Permutation.Permutation nat (x :: l) (y :: r))).
+idtac "Assumptions:".
+Abort.
+Print Assumptions select_perm.
+Goal True.
+idtac " ".
+
+idtac "-------------------  selection_sort_perm  --------------------".
+idtac " ".
+
+idtac "#> selection_sort_perm".
+idtac "Possible points: 3".
+check_type @selection_sort_perm (
+(forall l : list nat, @Permutation.Permutation nat l (selection_sort l))).
+idtac "Assumptions:".
+Abort.
+Print Assumptions selection_sort_perm.
+Goal True.
+idtac " ".
+
+idtac "-------------------  select_smallest  --------------------".
+idtac " ".
+
+idtac "#> select_smallest".
+idtac "Possible points: 3".
+check_type @select_smallest (
+(forall (x : nat) (al : list nat) (y : nat) (bl : list nat),
+ select x al = (y, bl) -> @Forall nat (fun z : nat => y <= z) bl)).
+idtac "Assumptions:".
+Abort.
+Print Assumptions select_smallest.
+Goal True.
+idtac " ".
+
+idtac "-------------------  selection_sort_sorted  --------------------".
+idtac " ".
+
+idtac "#> selection_sort_sorted".
+idtac "Possible points: 3".
+check_type @selection_sort_sorted ((forall al : list nat, sorted (selection_sort al))).
+idtac "Assumptions:".
+Abort.
+Print Assumptions selection_sort_sorted.
+Goal True.
+idtac " ".
+
+idtac "-------------------  selsort'_perm  --------------------".
+idtac " ".
+
+idtac "#> selsort'_perm".
+idtac "Possible points: 3".
+check_type @selsort'_perm (
+(forall (n : nat) (l : list nat),
+ @length nat l = n -> @Permutation.Permutation nat l (selsort' l))).
+idtac "Assumptions:".
+Abort.
+Print Assumptions selsort'_perm.
+Goal True.
+idtac " ".
+
+idtac " ".
+
+idtac "Max points - standard: 15".
+idtac "Max points - advanced: 15".
+idtac "".
+idtac "********** Summary **********".
+idtac "".
+idtac "********** Standard **********".
+idtac "---------- select_perm ---------".
+Print Assumptions select_perm.
+idtac "---------- selection_sort_perm ---------".
+Print Assumptions selection_sort_perm.
+idtac "---------- select_smallest ---------".
+Print Assumptions select_smallest.
+idtac "---------- selection_sort_sorted ---------".
+Print Assumptions selection_sort_sorted.
+idtac "---------- selsort'_perm ---------".
+Print Assumptions selsort'_perm.
+idtac "".
+idtac "********** Advanced **********".
+Abort.
+
+(* Fri Mar 15 17:09:53 UTC 2019 *)
